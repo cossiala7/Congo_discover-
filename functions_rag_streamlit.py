f0c22aaddaa01feb6a9625fcb948f4d9c61a3702
cossiala7 <167332492+cossiala7@users.noreply.github.com>
2026-02-13 15:58:15 +0000

    Modify embedding model and chat model initialization
    Updated embedding model to use 'models/text-embedding-001' and adjusted the chat model configuration.

--- functions_rag_streamlit.py
+++ functions_rag_streamlit.py
@@ -18,7 +18,7 @@ VECTOR_DB_PATH = "faiss_index_congo"
 # --- INITIALISATION DES MODÈLES ---
 # Gemini pour les vecteurs (Embeddings)
 embedding_model = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004", 
+    model="models/text-embedding-001", 
     google_api_key=GOOGLE_API_KEY
 )
 
@@ -141,3 +141,4 @@ Tu es un analyste expert du Congo-Brazzaville. Réponds uniquement en utilisant 
 
     response = chat_model.invoke(messages)
     return response.content
+
